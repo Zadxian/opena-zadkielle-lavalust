00460708ac21b6c18e3be6d32256993e6760911d
Zadkie Opena <zadxian1@gmail.com>
2026-08-18 22:40:47 +0800

    yes

--- app/views/student_profile.php
+++ app/views/student_profile.php
@@ -99,8 +99,8 @@
 </head>
 <body>
     <nav>
-        <a href="<?= site_url('/student') ?>">Home</a>
-        <a href="<?= site_url('/student/profile') ?>">Student Profile</a>
+        <a href="<?= site_url('/') ?>">Home</a>
+        <a href="<?= site_url('/student') ?>">Student Profile</a>
     </nav>
 
     <div class="card">
